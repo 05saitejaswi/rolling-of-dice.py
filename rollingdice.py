7ed8aa6717a8df3868aa971ffb735f06963a9220
chakravaram.saitejaswi <50818486+05saitejaswi@users.noreply.github.com>
2019-12-14 23:45:17 +0530

    rollingdice.py
    rollingdice

--- rollingdice.py
+++ rollingdice.py
@@ -6,7 +6,7 @@ roll_again = "yes"
 
 while roll_again == "yes" or roll_again == "y":
     print "Rolling the dices..."
-    print "The values are...."
+    print "The value are...."
     print random.randint(min, max)
     print random.randint(min, max)
 
